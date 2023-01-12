11472f5ed56343ed44e1d0ee1ba7f9c5f633aef6
AkamQadiri <akamq@hotmail.com>
2023-01-12 06:31:10 +0100

    Removed sb-wallpaper

--- config.h
+++ config.h
@@ -3,11 +3,10 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-internet",	5,	26),
-	BLOCK("sb-cpu",		10,	27),
-	BLOCK("sb-memory",	1,	28),
-	BLOCK("sb-disk",	5,	29),
-	BLOCK("sb-volume",	0,	1),
-	BLOCK("sb-wallpaper",	0,	24),
-	BLOCK("sb-clock",	1,	30),
+    BLOCK("sb-internet",  5,    26),
+    BLOCK("sb-cpu",       10,   27),
+    BLOCK("sb-memory",    1,    28),
+    BLOCK("sb-disk",      5,    29),
+    BLOCK("sb-volume",    0,    1),
+    BLOCK("sb-clock",     1,    30),
 };
