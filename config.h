ee4aec9b0f1c1b2194cdf603baf4a5cef00be879
AkamQadiri <akamq@hotmail.com>
2023-05-16 19:10:18 +0200

    Updated timing

--- config.h
+++ config.h
@@ -3,10 +3,10 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-    BLOCK("sb-internet",  5,    26),
-    BLOCK("sb-cpu",       10,   27),
-    BLOCK("sb-memory",    1,    28),
-    BLOCK("sb-disk",      5,    29),
+    BLOCK("sb-internet",  3,    26),
+    BLOCK("sb-cpu",       2,   27),
+    BLOCK("sb-memory",    2,    28),
+    BLOCK("sb-disk",      3,    29),
     BLOCK("sb-volume",    0,    1),
     BLOCK("sb-clock",     1,    30),
 };
